27eb5e86fc084524115e621ffdaf9c21807253a9
calocalize <calocalize@bb095895-b73e-0410-83e7-b5c1ea77f707>
2007-11-22 12:06:27 +0000

    Added tiny lzma encoder/decoder

--- CPatch/CPatchHeader.dpr
+++ CPatch/CPatchHeader.dpr
@@ -14,7 +14,17 @@ uses
   ComCtl32 in 'ComCtl32.pas',
   CommDlg in 'CommDlg.pas',
   Header in 'Header.pas',
-  Patcher in 'Patcher.pas';
+  Patcher in 'Patcher.pas',
+  UBufferedFS in 'LZMA\UBufferedFS.pas',
+  uClass in 'LZMA\uClass.pas',
+  UCRC in 'LZMA\UCRC.pas',
+  ULZMADec in 'LZMA\ULZMADec.pas',
+  ULZOutWindow in 'LZMA\compression\LZ\ULZOutWindow.pas',
+  ULZMABase in 'LZMA\compression\LZMA\ULZMABase.pas',
+  ULZMACommon in 'LZMA\compression\LZMA\ULZMACommon.pas',
+  ULZMADecoder in 'LZMA\compression\LZMA\ULZMADecoder.pas',
+  UBitTreeDecoder in 'LZMA\compression\RangeCoder\UBitTreeDecoder.pas',
+  URangeDecoder in 'LZMA\compression\RangeCoder\URangeDecoder.pas';
 
 begin
   Header_Main;
--- CPatch/CPatchMaker.dpr
+++ CPatch/CPatchMaker.dpr
@@ -14,7 +14,19 @@ uses
   ComCtl32 in 'ComCtl32.pas',
   CommDlg in 'CommDlg.pas',
   PatchGen in 'PatchGen.pas',
-  UpdateIcon in 'UpdateIcon.pas';
+  UpdateIcon in 'UpdateIcon.pas',
+  UBufferedFS in 'LZMA\UBufferedFS.pas',
+  uClass in 'LZMA\uClass.pas',
+  UCRC in 'LZMA\UCRC.pas',
+  ULZBinTree in 'LZMA\compression\LZ\ULZBinTree.pas',
+  ULZInWindow in 'LZMA\compression\LZ\ULZInWindow.pas',
+  ULZMABase in 'LZMA\compression\LZMA\ULZMABase.pas',
+  ULZMACommon in 'LZMA\compression\LZMA\ULZMACommon.pas',
+  ULZMAEncoder in 'LZMA\compression\LZMA\ULZMAEncoder.pas',
+  UBitTreeEncoder in 'LZMA\compression\RangeCoder\UBitTreeEncoder.pas',
+  URangeEncoder in 'LZMA\compression\RangeCoder\URangeEncoder.pas',
+  ULZMAEnc in 'LZMA\ULZMAEnc.pas',
+  URangeDecoder in 'LZMA\compression\RangeCoder\URangeDecoder.pas';
 
 begin
   Maker_Main;
